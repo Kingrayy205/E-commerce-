63a30425ef827413898ae4df2eaccba6ecb7cb97
Ubong <okokonubong3@gmail.com>
2025-09-10 00:59:20 +0100

    oldskull

--- script/index.js
+++ script/index.js
@@ -3,63 +3,63 @@
 
 
 
-// // homepage.js
+// homepage.js
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   loadCategories();
-//   loadFeaturedProducts();
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  loadCategories();
+  loadFeaturedProducts();
+});
 
-// // Load categories
-// function loadCategories() {
-//   fetch("https://api.escuelajs.co/api/v1/categories?limit=12")
-//     .then(res => res.json())
-//     .then(categories => {
-//       const categorycarousel = document.querySelector(".categoriescarousel");
-//       categorycarousel.innerHTML = "";
-//       categories.forEach(cat => {
-//         const div = document.createElement("div");
-//         div.classList.add("categorycontainer");
-//         div.style.padding = "0.3em"
-//         div.innerHTML = `
-//           <img src="${cat.image}" alt="${cat.name}" class="categoryproduct"/>
-//           <h3>${cat.name}</h3>
-//         `;
-//         categorycarousel.appendChild(div);
-//       });
-//     })
-//     .catch(err => console.error("Error loading categories:", err));
-// }
+// Load categories
+function loadCategories() {
+  fetch("https://api.escuelajs.co/api/v1/categories?limit=12")
+    .then(res => res.json())
+    .then(categories => {
+      const categorycarousel = document.querySelector(".categoriescarousel");
+      categorycarousel.innerHTML = "";
+      categories.forEach(cat => {
+        const div = document.createElement("div");
+        div.classList.add("categorycontainer");
+        div.style.padding = "0.3em"
+        div.innerHTML = `
+          <img src="${cat.image}" alt="${cat.name}" class="categoryproduct"/>
+          <h3>${cat.name}</h3>
+        `;
+        categorycarousel.appendChild(div);
+      });
+    })
+    .catch(err => console.error("Error loading categories:", err));
+}
 
-// // Load featured products
-// function loadFeaturedProducts() {
-//   fetch("https://api.escuelajs.co/api/v1/products?offset=0&limit=8")
-//     .then(res => res.json())
-//     .then(products => {
-//       const productcarousel = document.querySelector(".products-carousel");
-//       productcarousel.innerHTML = "";
-//       products.forEach(prod => {
-//         const div = document.createElement("div");
-//         div.classList.add("productcontainer");
-//         div.style.padding = "0.3em"
-//         div.innerHTML = `
-//           <img src="${prod.image}" alt="${prod.title}">
-//           <h3>${prod.title}</h3>
-//           <p>$${prod.price}</p>
-//           <br>
-//           <a href="pages/product.html?id=${prod.id}" class="btn">View Details</a>
-//         `;
-//         productcarousel.appendChild(div);
-//       });
-//     })
-//     .catch(err => console.error("Error loading featured products:", err));
-// }
-
-
+// Load featured products
+function loadFeaturedProducts() {
+  fetch("https://api.escuelajs.co/api/v1/products?offset=0&limit=8")
+    .then(res => res.json())
+    .then(products => {
+      const productcarousel = document.querySelector(".products-carousel");
+      productcarousel.innerHTML = "";
+      products.forEach(prod => {
+        const div = document.createElement("div");
+        div.classList.add("productcontainer");
+        div.style.padding = "0.3em"
+        div.innerHTML = `
+          <img src="${prod.image}" alt="${prod.title}" class="product">
+          <h3>${prod.title}</h3>
+          <p>$${prod.price}</p>
+          <br>
+          <a href="pages/product.html?id=${prod.id}" class="btn">View Details</a>
+        `;
+        productcarousel.appendChild(div);
+      });
+    })
+    .catch(err => console.error("Error loading featured products:", err));
+}
 
 
-// cartcount = document.querySelectorAll(".cart-count");
-// cartcount.forEach(count => count.textContent = 0);
+
+
+cartcount = document.querySelectorAll(".cart-count");
+cartcount.forEach(count => count.textContent = 0);
 
 
 // // homepage.js
@@ -171,56 +171,56 @@
 
 
 
-// homepage.js
+// // homepage.js
 
-document.addEventListener("DOMContentLoaded", () => {
-  loadCategories();
-  loadFeaturedProducts();
-});
-
-
+// document.addEventListener("DOMContentLoaded", () => {
+//   loadCategories();
+//   loadFeaturedProducts();
+// });
 
 
-// Load categories
-function loadCategories() {
-  fetch("https://api.escuelajs.co/api/v1/categories?limit=12")
-    .then(res => res.json())
-    .then(categories => {
-      const categorycarousel = document.querySelector(".categoriescarousel");
-      categorycarousel.innerHTML = "";
-      categories.forEach(cat => {
-        const div = document.createElement("div");
-        div.classList.add("categorycontainer");
-        div.innerHTML = `
-          <img src="${cat.image}" alt="${cat.name}" class="categoryproduct"/>
-          <h3>${cat.name}</h3>
-        `;
-        categorycarousel.appendChild(div);
-      });
-    })
-    .catch(err => console.error("Error loading categories:", err));
-}
 
-// Load featured products
-function loadFeaturedProducts() {
-  fetch("https://api.escuelajs.co/api/v1/products?offset=0&limit=8")
-    .then(res => res.json())
-    .then(products => {
-      const productcarousel = document.querySelector(".products-carousel");
-      productcarousel.innerHTML = "";
-      products.forEach(prod => {
-        const div = document.createElement("div");
-        div.classList.add("productcontainer");
-        div.innerHTML = `
-          <img src="${prod.image}" alt="${prod.title}" class="product" />
-          <h3>${prod.title}</h3>
-          <p>$${prod.price}</p>
-          <br>
-          <a href="pages/product.html?id=${prod.id}" class="btn">View Details</a>
-        `;
-        productcarousel.appendChild(div);
-      });
-    })
-    .catch(err => console.error("Error loading featured products:", err));
+
+// // Load categories
+// function loadCategories() {
+//   fetch("https://api.escuelajs.co/api/v1/categories?limit=12")
+//     .then(res => res.json())
+//     .then(categories => {
+//       const categorycarousel = document.querySelector(".categoriescarousel");
+//       categorycarousel.innerHTML = "";
+//       categories.forEach(cat => {
+//         const div = document.createElement("div");
+//         div.classList.add("categorycontainer");
+//         div.innerHTML = `
+//           <img src="${cat.image}" alt="${cat.name}" class="categoryproduct"/>
+//           <h3>${cat.name}</h3>
+//         `;
+//         categorycarousel.appendChild(div);
+//       });
+//     })
+//     .catch(err => console.error("Error loading categories:", err));
+// }
+
+// // Load featured products
+// function loadFeaturedProducts() {
+//   fetch("https://api.escuelajs.co/api/v1/products?offset=0&limit=8")
+//     .then(res => res.json())
+//     .then(products => {
+//       const productcarousel = document.querySelector(".products-carousel");
+//       productcarousel.innerHTML = "";
+//       products.forEach(prod => {
+//         const div = document.createElement("div");
+//         div.classList.add("productcontainer");
+//         div.innerHTML = ` 
+//           <img src="${prod.image}" alt="${prod.title}"  class="product" >
+//           <h3>${prod.title}</h3>
+//           <p>$${prod.price}</p>
+//           <br>
+//           <a href="pages/product.html?id=${prod.id}" class="btn">View Details</a>
+//         `;
+//         productcarousel.appendChild(div);
+//       });
+//     })
+//     .catch(err => console.error("Error loading featured products:", err));
     
-}
+// }
